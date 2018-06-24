e28575fcb4664a5f385325b7cb39561b2609ed90
Alex Tsvetanov <alex.tsvetanov.2002@gmail.com>
2018-06-24 14:17:48 +0300

    ajax and colors in the calendar

--- static/js/calendarViews/fcInitor.js
+++ static/js/calendarViews/fcInitor.js
@@ -1,24 +1,62 @@
-var dbMediator = require("../dbConnector/getData");
-
 $(document).ready(function () {
-    dbMediator.useUserEvents(function (data) {
-        let today = new Date();
-        $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'//,listWeek
-            },
-            defaultDate: new Date().format('Y-m-d'),
-            navLinks: true, // can click day/week names to navigate views
-            editable: false,
-            eventLimit: true, // allow "more" link when too many events
-            events: data,
-            eventRender: function (event, element) {
-                element.click(function () {
-                    $("")
-                });
+    let today = new Date();
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay,agendaThreeDay'//,listWeek
+        },
+
+        views: {
+            agendaThreeDay: {
+                type: 'agenda',
+                duration: { days: 3 },
+                buttonText: '3 day'
             }
-        });
+        },
+
+        defaultDate: today,
+        navLinks: true, // can click day/week names to navigate views
+        editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        events: function (start, end, timezone, callback) {
+            //dbMediator.useFakeEvents(callback);/*
+            $.ajax({
+                url: '192.168.166.223:3000/event',
+                dataType: 'application/json',
+                contentType: "application/x-www-form-urlencoded; charset=utf-8",
+                data: {
+                    start: start.toISOString(),
+                    end: end.toISOString()
+                },
+                success: function (events) {
+                    callback(events);
+                }
+            });//*/
+        },
+        eventRender: function (event, element) {
+            if (event.type === 4) {
+                element.css("background-color", "red");
+                element.css("color", "white");
+            }
+            else if (event.type === 3) {
+                element.css("background-color", "green");
+                element.css("color", "white");
+            }
+            else if(event.type === 2) {
+                element.css("background-color", "blue");
+                element.css("color", "white");
+            }
+            else if (event.type === 1)
+                element.css("background-color", "yellow");
+            else if (event.type === 0)
+                element.css("background-color", "cyan");
+
+            element.click(function () {
+                $(".modal h5").html(event.title);
+                $(".modal .modal-body").html(event.description);
+                $(".modal").show();
+            });
+        }
     });
 });
--- static/js/dbConnector/getData.js
+++ static/js/dbConnector/getData.js
@@ -1,13 +1,110 @@
-(function () {
-    return {
-        useUserEvents: function (handler) {
-            $.ajax({
-                dataType: "json",
-                url: "/events/get",
-                data: {},
-                success: function (data) {
-                    handler(data);
-                });
-        }
-    };
-})();
+const dbMediator = {
+    useFakeEvents: function (handler) {
+        handler([
+            {
+                title: 'All Day Event',
+                start: '2018-03-01',
+                description: 'asdf',
+                type: 0
+            },
+            {
+                title: 'All Day Event',
+                start: '2018-03-02',
+                type: 1
+            },
+            {
+                title: 'All Day Event',
+                start: '2018-03-03',
+                type: 2
+            },
+            {
+                title: 'All Day Event',
+                start: '2018-03-04',
+                type: 3
+            },
+            {
+                title: 'All Day Event',
+                start: '2018-03-05',
+                type: 4
+            },
+            {
+                title: 'Long Event',
+                start: '2018-03-07',
+                end: '2018-03-10'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2018-03-09T16:00:00'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2018-03-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2018-03-11',
+                end: '2018-03-13'
+            },
+            {
+                title: 'Meeting',
+                start: '2018-03-12T10:30:00',
+                end: '2018-03-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2018-03-12T12:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2018-03-12T14:30:00'
+            },
+            {
+                title: 'Happy Hour',
+                start: '2018-03-12T17:30:00'
+            },
+            {
+                title: 'Dinner',
+                start: '2018-03-12T20:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                description: 'This is a cool event',
+                start: '2018-03-13T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2018-03-28'
+            }
+        ]);
+    },
+    useUserEvents: function (handler) {
+        $.ajax({
+            dataType: "json",
+            url: "http://192.168.166.223:3000/events/get",
+            data: {},
+            success: function (data) {
+                handler(data);
+            }
+        });
+    },
+    useUserEventsByInterval: function (start, end, handler) {
+        $.ajax({
+            url: 'http://192.168.166.223:3000/event',
+            dataType: 'application/json',
+            contentType: "application/x-www-form-urlencoded",
+            crossDomain: true,
+            data: {
+                start: start.toISOString(),
+                end: end.toISOString()
+            },
+            success: function (events) {
+                console.log(events.length);
+                console.log(events);
+                handler(events);
+            }
+        });
+    },
+};
