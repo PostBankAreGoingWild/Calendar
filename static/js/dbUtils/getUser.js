9de29217696b602d079cba369db83523b5694207
Ro6afF <dimo_chanev@windowslive.com>
2018-06-24 20:05:05 +0300

    fiex blq

--- static/js/dbUtils/getUser.js
+++ static/js/dbUtils/getUser.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
     $.ajax({
         url: '/user/email',
-        dataType: 'application/json',
         xhrFields: { withCredentials: true },
-        contentType: "application/x-www-form-urlencoded",
-        data: {
-        },
         async: false,
         success: function (data) {
+            console.log("USER:",data);
             if (data !== undefined) {
                 $("#login").hide();
                 $("#logout").show();
-                $("#user").html(data);
+                $("#user").html(', ' + data);
             }
         }
     });
