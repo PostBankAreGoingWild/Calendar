46ef3f7c4af4ed9d2ed26d15e3ca67652805b340
Ro6afF <dimo_chanev@windowslive.com>
2018-06-24 20:12:59 +0300

    asdilavb

--- static/js/dbUtils/getUser.js
+++ static/js/dbUtils/getUser.js
@@ -4,13 +4,7 @@ $(document).ready(function () {
         xhrFields: { withCredentials: true },
         async: false,
         success: function (data) {
-<<<<<<< HEAD
-            console.log(data);
             if (data) {
-=======
-            console.log("USER:",data);
-            if (data !== undefined) {
->>>>>>> 9de29217696b602d079cba369db83523b5694207
                 $("#login").hide();
                 $("#logout").show();
                 $("#user").html(', ' + data);
